20c934041d84aea1dee0dcf477dd017c5697f066
Navneet Singh <navneetsingh@deakin.edu.au>
2023-05-23 13:34:53 +1000

    added test

---
+++ server.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('./server'); // Assuming the server.js file is in the same directory
+
+describe('GET /hello', () => {
+  it('should return a welcome message', (done) => {
+    request(app)
+      .get('/hello')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.equal('Hello! Welcome to my calculator service.');
+        done();
+      });
+  });
+});
+
+
